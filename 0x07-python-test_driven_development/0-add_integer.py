db8d56c9ed3e71de742c1b2a0445314026f51444
Xclussiveog <ikireziremymagnom@gmail.com>
2024-02-01 02:59:33 -0800

    0-add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,30 +1,47 @@
 #!/usr/bin/python3
-"""
-
-This module is composed by a function that adds two numbers
 
 """
+Module with a function that adds two numbers. It checks for the validity of
+arguments given as operands before trying to add them. In the event of an
+error, an exception is raised. Notable amongst these exceptions is the
+TypeError which is raised when the operands fail to be an integer.
+"""
 
 
-def add_integer(a, b=98):
-    """ Function that adds two integer and/or float numbers
+def add_integer(a: "float | int", b: "float | int" = 98) -> int:
+    """Returns the sum of two integers.
 
     Args:
-        a: first number
-        b: second number
-
-    Returns:
-        The addition of the two given numbers
+        a (float | int): The first number.
+        b (float | int, optional): The second number. Defaults to 98.
 
     Raises:
-        TypeError: If a or b aren't integer and/or float numbers
+        TypeError: When the operands given are not of a valid type (integers).
 
+    Returns:
+        int: The sum of the two numbers as an integer.
+
+    Tests:
+        >>> add_integer(0)
+        98
+        >>> add_integer(1, -2)
+        -1
+        >>> add_integer('1', '67')
+        Traceback (most recent call last):
+        ...
+        TypeError: a must be an integer
     """
-
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return (a + b)
+
+    # ensure the operands are of integer type
+    try:
+        int(a)
+        int(b)
+    except Exception as err:
+        raise err
+
+    return int(a) + int(b)
